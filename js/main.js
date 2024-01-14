88259591ef5708956ed1bb43f67645afaced9d0b
saykhulalam <saykhulalam@gmail.com>
2024-01-14 13:22:35 +0600

    responsite and desing all done

---
+++ js/main.js
@@ -0,0 +1,37 @@
+ $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: "<i class='fa-solid fa-angle-left'></i>",
+    nextArrow: "<i class='fa-solid fa-angle-right'></i>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
